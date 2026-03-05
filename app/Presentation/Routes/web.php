b8604abae90f58b7699c7b52dbb9c6ee61622a07
Aditya Pratama Febriono <adityapeef18@gmail.com>
2026-03-05 21:59:39 +0700

    change routes web

--- app/Presentation/Routes/web.php
+++ app/Presentation/Routes/web.php
@@ -8,23 +8,6 @@ use App\Presentation\Http\Controllers\Web\Auth\LogoutController;
 
 Route::get('/', fn() => redirect('/login'));
 
-// Route::get('/users', [UserWebController::class, 'index']);
-// Route::get('/users/create', [UserWebController::class, 'create']);
-// Route::post('/users', [UserWebController::class, 'store']);
-// Route::get('/users/{id}', [UserWebController::class, 'show']);
-// Route::get('/users/{id}/edit', [UserWebController::class, 'edit']);
-// Route::put('/users/{id}', [UserWebController::class, 'update']);
-// Route::delete('/users/{id}', [UserWebController::class, 'destroy']);
-
-// Route::get('/products', [ProductWebController::class, 'index']);
-// Route::get('/products/create', [ProductWebController::class, 'create']);
-// Route::post('/products', [ProductWebController::class, 'store']);
-// Route::get('/products/{id}', [ProductWebController::class, 'show']);
-// Route::get('/products/{id}/edit', [ProductWebController::class, 'edit']);
-// Route::put('/products/{id}', [ProductWebController::class, 'update']);
-// Route::delete('/products/{id}', [ProductWebController::class, 'destroy']);
-
-
 Route::get('/login', [LoginController::class, 'show'])->name('login');
 Route::post('/login', [LoginController::class, 'store']);
 Route::post('/logout', LogoutController::class);
